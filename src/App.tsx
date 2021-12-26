ab5cb231f63ff25d337819be3d6afd7b7030c8ee
will-johnston-goodyear <will_johnston@goodyear.com>
2021-12-26 11:17:12 -0500

    Adds React Router - closes issue #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+// Global Imports
 import * as React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+} from 'react-router-dom';
+
+// Local Imports
+import { Button } from 'stories/Button';
 
 const App = () => (
-		<span>App Works</span>
+	<>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<Routes>
+				<Route path="/" element={<Button label="Home" isMobile={false} onClick={() => { }} />} />
+			</Routes>
+		</Router>
+		<span>Test</span>
+	</>
 	);
 
 export default App;
