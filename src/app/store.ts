b16411a9d6576b41c6dc529e70aa1ad4a21282e2
Will Johnston <wijohnst@gmail.com>
2021-12-29 10:04:15 -0500

    Implements RTKQuery (#32)
    Co-authored-by: will-johnston-goodyear <will_johnston@goodyear.com>

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,6 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { sessionApi } from 'features/session/sessionApi';
 import sessionReducer from 'features/session/sessionSlice';
 import counterReducer from '../features/counter/counterSlice';
 
@@ -6,8 +8,13 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
     session: sessionReducer,
+    [sessionApi.reducerPath]: sessionApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sessionApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- src/stories/ProfilePage.tsx
+++ src/stories/ProfilePage.tsx
@@ -4,21 +4,26 @@ import * as React from 'react';
 // Local Imports
 import { useAppSelector } from 'app/hooks';
 import { selectSession } from 'features/session/sessionSlice';
+import { useGetUserQuery } from 'features/session/sessionApi';
 
 interface Props {}
 
 // eslint-disable-next-line no-empty-pattern
 export const ProfilePage = ({}: Props) => {
+  const { data, isLoading: queryIsLoading } = useGetUserQuery('userId');
   const { isAuthLoading, isAuthenticated, user } = useAppSelector((state) =>
     selectSession(state)
   );
 
-  if (isAuthLoading) {
+  if (isAuthLoading || queryIsLoading) {
     return <span>Loading...</span>;
   }
 
-  return isAuthenticated ? (
-    <span>Welcome {user?.name ?? 'User'}</span>
+  return isAuthenticated && data ? (
+    <>
+      <span>Welcome {user?.name ?? 'User'}</span>
+      <img src={data.profileURI} alt="profile" />
+    </>
   ) : (
     <span>Please Login</span>
   );
