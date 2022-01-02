9c4b1f423be3c12eff35385f9e705ee274f2aa40
Will Johnston <wijohnst@gmail.com>
2022-01-02 12:22:44 -0500

    Adds GrafHeader to app (#47)
    Co-authored-by: will-johnston-goodyear <will_johnston@goodyear.com>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 // Global Imports
 import { User } from '@auth0/auth0-react';
 
+// Local Imports
+import { AppLinkProps } from 'stories/AppLink';
+
 export const breakpoints: { [key: string]: string } = {
   mobile: '(min-width: 600px)',
   tablet: '(min-width: 1000px)',
@@ -10,3 +13,21 @@ export const breakpoints: { [key: string]: string } = {
 export const TestUser: User = {
   name: 'Test User',
 };
+
+export const NavLinks: Array<AppLinkProps> = [
+  {
+    linkPath: '/profile',
+    linkText: 'profile',
+    isActive: false,
+  },
+  {
+    linkPath: '/community',
+    linkText: 'community',
+    isActive: false,
+  },
+  {
+    linkPath: '/info',
+    linkText: 'info',
+    isActive: false,
+  },
+];
