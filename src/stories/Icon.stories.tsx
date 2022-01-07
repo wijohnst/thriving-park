116cb4eaf899d89c6a8407943875ecdd7c982b82
will-johnston-goodyear <will_johnston@goodyear.com>
2022-01-06 20:00:17 -0500

    Adds Icon component.

---
+++ src/stories/Icon.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Icon } from 'stories/Icon';
+import { defaultTheme } from 'themes';
+import { standardWrapper } from 'stories/decoratorStyles';
+import { ReactComponent as CaretIcon } from 'stories/assets/caret_icon.svg';
+import { ReactComponent as XIcon } from 'stories/assets/ex_icon.svg';
+
+export default {
+  title: 'Design System/Atoms/Icon',
+  component: Icon,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          backgroundColor: defaultTheme.primaryBlue,
+          ...standardWrapper,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Caret = Template.bind({});
+Caret.args = {
+  ...Default.args,
+  icon: <CaretIcon data-testid="caret-icon" />,
+};
+
+export const CustomOnClick = Template.bind({});
+CustomOnClick.args = {
+  ...Default.args,
+  // eslint-disable-next-line no-console
+  onClick: () => console.log('Custom on click...'),
+};
+
+export const ColoredIcon = Template.bind({});
+ColoredIcon.args = {
+  ...Default.args,
+  icon: <CaretIcon fill={defaultTheme.brightGreen} />,
+};
+
+export const CloseIcon = Template.bind({});
+CloseIcon.args = {
+  ...Default.args,
+  icon: <XIcon />,
+};
