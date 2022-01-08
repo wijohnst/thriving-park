47c19fefd899ae903b5ce8f26eb59bf2e9326844
will-johnston-goodyear <will_johnston@goodyear.com>
2022-01-08 12:57:51 -0500

    Updates styling based on InputContainer.

--- src/stories/Inputs/Input.style.ts
+++ src/stories/Inputs/Input.style.ts
@@ -4,13 +4,14 @@ import styled from 'styled-components';
 export const InputElement = styled.input<{
   isActive: boolean;
 }>`
-  border: solid thin;
+  border: solid;
 	border-color: ${(props) =>
     props.isActive ? `${props.theme.brightGreen};` : 'white;'}
   text-decoration: none;
   border-radius: 5px;
   outline: none;
-	padding: 0.5rem;
+	/* padding: 0.5rem; */
+	padding: 0.25rem 0 0.25rem 0.5rem;
 	margin: 0.25rem;
 	box-sizing: border-box;
 	width: 100%;
