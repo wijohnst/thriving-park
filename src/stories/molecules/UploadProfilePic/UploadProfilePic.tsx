ed070e9ceca4b78b4434fcb00a339690e15d3ed7
will-johnston-goodyear <will_johnston@goodyear.com>
2022-02-01 08:16:20 -0500

    Adds UploadProfilePic component

---
+++ src/stories/molecules/UploadProfilePic/UploadProfilePic.tsx
@@ -0,0 +1,16 @@
+// Global Imports
+import * as React from 'react';
+
+// Local Imports
+import { UploadProfilePicWrapper } from 'stories/molecules/UploadProfilePic/UploadProfilePic.style';
+
+interface Props {}
+
+// eslint-disable-next-line no-empty-pattern
+export const UploadProfilePic = ({}: Props) => {
+  return (
+    <UploadProfilePicWrapper>
+      <span>UploadProfilePic Works!</span>
+    </UploadProfilePicWrapper>
+  );
+};
