706a98f966bcdd24a47fd71163f40642522d2d05
willcary <w.a.c.ary@gmail.com>
2022-04-06 09:51:57 -0400

    Refactor tests for altered shape prop

--- src/stories/atoms/Shape/Shape.test.tsx
+++ src/stories/atoms/Shape/Shape.test.tsx
@@ -6,21 +6,16 @@ import { composeStories } from '@storybook/testing-react';
 import { render } from 'utils/test-utils';
 import * as stories from 'stories/atoms/Shape/Shape.stories';
 
-const { Default, Rectangle, Color } = composeStories(stories);
+const { Default, Rectangle } = composeStories(stories);
 
 describe('Shape unit tests', () => {
-  it('Should render the Default Shape component', () => {
+  it('Should render the square Shape component', () => {
     const { getByTestId } = render(<Default />);
-    expect(getByTestId('shape-component-default')).toBeInTheDocument();
+    expect(getByTestId('shape-component-square')).toBeInTheDocument();
   });
 
-  it('Should render the Rectangle Shape component', () => {
+  it('Should render the rectangle Shape component', () => {
     const { getByTestId } = render(<Rectangle />);
     expect(getByTestId('shape-component-rectangle')).toBeInTheDocument();
-  });
-
-  it('Should render the Color Shape component', () => {
-    const { getByTestId } = render(<Color />);
-    expect(getByTestId('shape-component-color')).toBeInTheDocument();
   });
 });
