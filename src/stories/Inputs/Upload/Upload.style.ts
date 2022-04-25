a6acce1b8028eefffe35d346e81d8fcd9d381133
Will Johnston <wijohnst@gmail.com>
2022-04-25 10:37:00 -0400

    Issue 133 - `Upload Component`  (#136)
    * Updates RTL dependencies.
    
    * Adds Upload component
    
    Co-authored-by: will-johnston-goodyear <will_johnston@goodyear.com>

--- src/stories/Inputs/Upload/Upload.style.ts
+++ src/stories/Inputs/Upload/Upload.style.ts
@@ -1,4 +1,31 @@
 // Global Imports
 import styled from 'styled-components';
 
-export const UploadWrapper = styled.div``;
+export const UploadWrapper = styled.div<{
+  isActive: boolean;
+}>`
+  border: solid;
+	border-color: ${(props) =>
+    props.isActive ? `${props.theme.brightGreen};` : 'white;'}
+	background-color: white;
+  border-radius: 5px;
+  outline: none;
+	padding: 0.25rem;
+  margin: 0.25rem;
+  box-sizing: border-box;
+  width: 100%;
+	font-family: ${(props) => props.theme.primaryFontFamily};
+	display: flex;
+	flex-flow: row nowrap;
+`;
+
+export const UploadText = styled.span`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  font-style: italic;
+`;
+
+export const UploadIconWrapper = styled.div``;
+
+export const UploadElementWrapper = styled.div``;
