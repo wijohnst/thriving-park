75bfad825cb041463aa8d709ca82680acc70ac29
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-11-08 10:07:56 -0500

    Current state of Dropdown Molecule

---
+++ src/stories/molecules/Dropdown/Dropdown.tsx
@@ -0,0 +1,36 @@
+// Global Imports
+import React, { ReactNode } from 'react';
+
+// Local Imports
+// import { DropdownDiv } from './Dropdown.style';
+// import { defaultTheme } from 'themes';
+import { Button } from 'stories/atoms/Button/Button';
+// I know the caret has to wrapped in a wrapper so we can adjust it's placement and allow for roatation, but my first goal was just to render the Caret somewhere on the button first
+// import { DropCaretWrapper } from 'stories/molecules/Dropdown/Dropdown.style';
+// import { SvgContainer } from 'stories/atoms/SVGIcon/svgicon-styles';
+import { ReactComponent as CaretSvg } from 'stories/assets/caret_icon.svg';
+
+interface Props {
+  isMobile?: boolean;
+  // orientation?: string;
+  label?: string;
+  onClick?: () => void;
+  children?: ReactNode; 
+}
+
+export const Dropdown = ({
+  isMobile = false,
+  label = 'DROPDOWN BUTTON',
+  onClick: handleClick = () => console.log('Click!'),
+  children = <CaretSvg/>
+
+}: Props) => {
+    <Button 
+      isMobile={isMobile} 
+      label={label} 
+      onClick={() => handleClick()} 
+    >
+      {children}
+    </Button>
+  );
+};
